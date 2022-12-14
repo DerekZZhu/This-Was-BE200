abe18a9b30f162b2e14ed4cfceb6b4e51daf9d53
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2022-12-14 00:50:45 -0800

    Made simple nav

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 
 import Map from './components/Map.jsx'
 import Home from './pages/Home.js'
+import Nav from './components/Nav.jsx'
 
 import data from './data.json'
 import COLORS from './COLORS.json'
@@ -10,23 +11,24 @@ import COLORS from './COLORS.json'
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route path="/home">
-          <Home/>
-        </Route>
+    {/* //   <Switch>
+    //     <Route path="/home">
+    //       <Home/>
+    //     </Route>
 
-        <Route path="/guides" exact>
+    //     <Route path="/guides" exact>
 
-        </Route>
+    //     </Route>
 
-        <Route path="/guides/san-fran">
-          <Map data={data} theme={COLORS.rainbow}/>
-        </Route>
+    //     <Route path="/guides/san-fran">
+    //       <Map data={data} theme={COLORS.rainbow}/>
+    //     </Route>
 
-        <Route path="/guides/:mapId">
+    //     <Route path="/guides/:mapId">
 
-        </Route>
-      </Switch>
+    //     </Route>
+    //   </Switch> */}
+      <Nav/>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,19 @@
+import './Nav.css'
+import {Link} from 'react-router-dom'
 const Nav = () => {
+    const user = null;
     return(
-        <div>
-            
+        <div className='navbar'>
+            <Link to='/home'>HOME</Link>
+            <Link to='/guides'>EXPLORE</Link>
+            <Link to='/signup'>LOGIN</Link>
+
+            {
+                user && (
+                    <Link to='/create'>CREATE</Link>
+                )
+            }
+            <Link to='/account' className='link'><div className='account'></div></Link>
         </div>
     )
 }
