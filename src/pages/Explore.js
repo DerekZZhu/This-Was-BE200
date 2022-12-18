2e3e4cef5f01a903c9202c08ba4ff3823acd224a
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2022-12-17 18:14:23 -0800

    Frameworked explore page

---
+++ src/pages/Explore.js
@@ -0,0 +1,20 @@
+
+const Explore = () => {
+    return(
+        <div>
+            <section className="top-picks">
+
+            </section>
+            <section className="content">
+                <div className="filter">
+
+                </div>
+                <div className="grid">
+
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Explore
