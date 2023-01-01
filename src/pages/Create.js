240897e1cf03d11200fff119404662020852a23e
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2023-01-01 15:15:59 -0800

    Added dynamic waypoint ui

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,26 +1,33 @@
-import api_key from "../env.js"
+import gaypi_key from "../env.js"
 import { useState, useRef } from "react"
 import { useJsApiLoader, GoogleMap, Marker, Autocomplete, DirectionsRenderer } from "@react-google-maps/api"
+import { FaPlus } from "react-icons/fa"
 
 import style from "./Create.module.css"
 
 
 const center = {lat:47.6553, lng:-122.3035}
 const libraries = ['places']
-const Create = () => {
+
+const Create = (props) => {
     const {isLoaded} = useJsApiLoader({
-        googleMapsApiKey: api_key,
+        googleMapsApiKey: gaypi_key,
         libraries 
     })
     const google = window.google;
 
     const [map, setMap] = useState(null)
     const [directionResponse, setDirectionResponse] = useState(null)
+
     const [distance, setDistance] = useState('')
     const [duration, setDuration] = useState('')
 
+    const [wayPoints, setWayPoints] = useState([])
+    const [count, setCount] = useState(0)
+
     const originRef = useRef()
     const destinationRef = useRef()
+    const containerRef = useRef([])
 
     if (!isLoaded) {
         console.log("not loaded");
@@ -43,19 +50,55 @@ const Create = () => {
         setDuration(results.routes[0].legs[0].duration.text)
     }
 
+    const handleInputChange = (index, event) => {
+        let data = [...wayPoints]
+        data[index] = document.getElementById(index).value
+        setWayPoints(data);
+    }
+
+    const addNewStop = () => {
+        if (wayPoints.length !=0 && document.getElementById(`${count-1}`).value === "") {
+            return
+        }
+
+        setCount(count+1)
+        let data = [...wayPoints]
+        for(var i = 0; i < count; i++) {
+            data[i] = document.getElementById(`${i}`).value
+        }
+        setWayPoints([...data, ''])
+    }
+
+    console.log(count);
+    console.log(wayPoints);
+    // console.log(document.getElementById(1).value);
     return(
         <div className={style.create_page} style={{height:'88vh'}}>
             <div className={style.dashboard}>
                 <div className={style.form}>
                         <Autocomplete>
-                            <input type="text" id="from" ref={originRef}/>
+                            <input type="text" id="from" ref={originRef} placeholder="Enter Starting Location"/>
+                        </Autocomplete>
+                        {wayPoints.map((input, index) => {
+                                return(
+                                    <Autocomplete key={index} >
+                                        <input type="text" id={index} placeholder="Enter Stop"/>
+                                    </Autocomplete>
+                                )
+                            })
+                        }
+                        <Autocomplete>
+                            <input type="text" id="from" ref={destinationRef} placeholder="Enter Ending Location"/>
                         </Autocomplete>
 
-                        <Autocomplete>
-                            <input type="text" id="to" ref={destinationRef}/>
-                        </Autocomplete>
-                    <button onClick={calculateRoute}>Calculate Route</button>
+                        <div className={style.add} onClick={addNewStop}>
+                            <FaPlus/>
+                        </div>
                 </div>
+                <div className={style.block}/>
+                <button onClick={calculateRoute}>Calculate Route</button>
+                <h3>Distance: {distance}</h3>
+                <h3>Duration: {duration}</h3>
             </div>
 
             <GoogleMap 
